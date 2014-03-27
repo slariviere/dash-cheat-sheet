65f890d261154d6f58924752528352a3aa16916f
Sébastien Larivière <sebastien@lariviere.me>
2014-03-26 21:12:17 -0400

    Init osx

---
+++ gen.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/usr/local/Cellar/ruby193/1.9.3-p484/bin/cheatset generate $1
---
+++ osx.rb
@@ -0,0 +1,29 @@
+cheatsheet do
+  title 'OSX'                # Will be displayed by Dash in the docset list
+  docset_file_name 'OSX'  # Used for the filename of the docset
+  keyword 'osx'              # Used as the initial search keyword (listed in Preferences > Docsets)
+
+  introduction 'OSX Keybords shortcuts'  # Optional, can contain Markdown or HTML
+
+  # A cheat sheet must consist of categories
+  category do
+    id 'Screenshot'  # Must be unique and is used as title of the category
+
+    entry do
+      command 'Command-Shift-3'
+      name ' Complete screen, saves to desktop'
+    end
+    entry do
+      command 'Command-Shift-4'
+      name 'Take a screenshot of an area, saves to desktop'
+    end
+  entry do
+      command 'Command-Control-Shift-3'
+      name 'Take a screenshot of the screen to clipboard'
+    end
+  entry do
+     command 'Command-Control-Shift-4'
+     name 'Take a screenshot of an area to the clipboard'
+  end
+  end
+end
